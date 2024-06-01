152c3846e5af34f56b1951c677e86715ece01734
Jocelin Cailloux <jocelin.cailloux@gmail.com>
2024-06-01 12:05:25 +0200

    First working version when using only the purse (coins should not be considered in the inventory)

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,145 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.ibm.icu.util.MeasureUnit;
+import net.minecraft.entity.player.HungerManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import wraith.fwaystones.FabricWaystones;
+import wraith.fwaystones.access.WaystoneValue;
+import wraith.fwaystones.block.WaystoneBlockEntity;
+import wraith.fwaystones.util.FWConfigModel;
+import wraith.fwaystones.util.TeleportSources;
+import wraith.fwaystones.util.Utils;
 
-@Mixin(MinecraftServer.class)
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static java.lang.Math.min;
+import static net.minecraft.util.math.MathHelper.floor;
+import static wraith.fwaystones.util.Utils.*;
+import net.minecraft.registry.Registries;
+
+import com.glisco.numismaticoverhaul.ModComponents;
+
+@Mixin(Utils.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	private static Identifier copperId = new Identifier("numismatic-overhaul", "copper_coin");
+	private static Identifier silverIO = new Identifier("numismatic-overhaul", "silver_coin");
+	private static Identifier goldId = new Identifier("numismatic-overhaul", "gold_coin");
+	private static boolean useNumismatic = getTeleportCostItem().equals(copperId) || getTeleportCostItem().equals(silverIO) || getTeleportCostItem().equals(goldId);
+	private static int itemFactor = getTeleportCostItem().equals(copperId) ? 1 : getTeleportCostItem().equals(silverIO) ? 100 : 10000;
+
+	/**
+	 * @author Galysso
+	 * @reason Ass ability to check coins from numismatic-overhaul purse
+	 */
+	@Overwrite
+	public static boolean containsItem(PlayerInventory inventory, Item item, int maxAmount) {
+		Identifier itemId = getTeleportCostItem();
+		if (useNumismatic) {
+			return maxAmount*itemFactor <= (int) ModComponents.CURRENCY.get(inventory.player).getValue();
+		} else {
+			int amount = 0;
+			Iterator var4 = inventory.main.iterator();
+
+			ItemStack stack;
+			while (var4.hasNext()) {
+				stack = (ItemStack) var4.next();
+				if (stack.getItem().equals(item)) {
+					amount += stack.getCount();
+				}
+			}
+
+			var4 = inventory.offHand.iterator();
+
+			while (var4.hasNext()) {
+				stack = (ItemStack) var4.next();
+				if (stack.getItem().equals(item)) {
+					amount += stack.getCount();
+				}
+			}
+
+			var4 = inventory.armor.iterator();
+
+			while (var4.hasNext()) {
+				stack = (ItemStack) var4.next();
+				if (stack.getItem().equals(item)) {
+					amount += stack.getCount();
+				}
+			}
+
+			return amount >= maxAmount;
+		}
+	}
+
+	/**
+	 * @author Galysso
+	 * @reason Adding ability to remove coins from numismatic-overhaul purse
+	 */
+	@Overwrite
+	public static void removeItem(PlayerInventory inventory, Item item, int totalAmount) {
+		/* check the player has enough in their purse first (what the player has on hand might be voluntarily kept on hand) */
+		Identifier itemId = getTeleportCostItem();
+		if (useNumismatic) {
+			ModComponents.CURRENCY.get(inventory.player).modify(-itemFactor*totalAmount);
+		} else {
+			Iterator var3 = inventory.main.iterator();
+
+			while (totalAmount > 0) {
+				ItemStack stack;
+				int amount;
+				if (!var3.hasNext()) {
+					var3 = inventory.offHand.iterator();
+
+					do {
+						if (!var3.hasNext()) {
+							var3 = inventory.armor.iterator();
+
+							do {
+								if (!var3.hasNext()) {
+									return;
+								}
+
+								stack = (ItemStack) var3.next();
+								if (stack.getItem().equals(item)) {
+									amount = stack.getCount();
+									stack.decrement(totalAmount);
+									totalAmount -= amount;
+								}
+							} while (totalAmount > 0);
+
+							return;
+						}
+
+						stack = (ItemStack) var3.next();
+						if (stack.getItem().equals(item)) {
+							amount = stack.getCount();
+							stack.decrement(totalAmount);
+							totalAmount -= amount;
+						}
+					} while (totalAmount > 0);
+
+					return;
+				}
+
+				stack = (ItemStack) var3.next();
+				if (stack.getItem().equals(item)) {
+					amount = stack.getCount();
+					stack.decrement(totalAmount);
+					totalAmount -= amount;
+				}
+			}
+		}
 	}
 }
---
+++ src/main/java/com/example/mixin/NumismaticOverhaulMixin.java
@@ -0,0 +1,85 @@
+package com.example.mixin;
+
+import com.glisco.numismaticoverhaul.NumismaticCommand;
+import com.glisco.numismaticoverhaul.NumismaticOverhaul;
+import com.glisco.numismaticoverhaul.block.NumismaticOverhaulBlocks;
+import com.glisco.numismaticoverhaul.currency.MoneyBagLootEntry;
+import com.glisco.numismaticoverhaul.item.NumismaticOverhaulItems;
+import com.glisco.numismaticoverhaul.network.RequestPurseActionC2SPacket;
+import com.glisco.numismaticoverhaul.network.ShopScreenHandlerRequestC2SPacket;
+import com.glisco.numismaticoverhaul.network.UpdateShopScreenS2CPacket;
+import com.glisco.numismaticoverhaul.villagers.data.VillagerTradesResourceListener;
+import com.glisco.numismaticoverhaul.villagers.json.VillagerTradesHandler;
+import io.wispforest.owo.ops.LootOps;
+import io.wispforest.owo.registration.reflect.FieldRegistrationHandler;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.resource.ResourceType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import static com.glisco.numismaticoverhaul.NumismaticOverhaul.*;
+
+@Mixin(value = NumismaticOverhaul.class)
+public class NumismaticOverhaulMixin {
+
+    /**
+     * @author Galysso
+     * @reason Register all the things
+     */
+    /*@Overwrite
+    public void onInitialize() {
+        FieldRegistrationHandler.register(NumismaticOverhaulItems.class, MOD_ID, false);
+        FieldRegistrationHandler.register(NumismaticOverhaulBlocks.class, MOD_ID, false);
+        FieldRegistrationHandler.register(NumismaticOverhaulBlocks.Entities.class, MOD_ID, false);
+
+        Registry.register(Registries.SOUND_EVENT, PIGGY_BANK_BREAK.getId(), PIGGY_BANK_BREAK);
+        Registry.register(Registries.LOOT_POOL_ENTRY_TYPE, id("money_bag"), MONEY_BAG_ENTRY);
+
+        Registry.register(Registries.SCREEN_HANDLER, id("shop"), SHOP_SCREEN_HANDLER_TYPE);
+        Registry.register(Registries.SCREEN_HANDLER, id("piggy_bank"), PIGGY_BANK_SCREEN_HANDLER_TYPE);
+
+        CHANNEL.registerServerbound(RequestPurseActionC2SPacket.class, RequestPurseActionC2SPacket::handle);
+        CHANNEL.registerServerbound(ShopScreenHandlerRequestC2SPacket.class, ShopScreenHandlerRequestC2SPacket::handle);
+        UpdateShopScreenS2CPacket.initialize();
+
+        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(new VillagerTradesResourceListener());
+        VillagerTradesHandler.registerDefaultAdapters();
+
+        CommandRegistrationCallback.EVENT.register(NumismaticCommand::register);
+
+        ServerLifecycleEvents.END_DATA_PACK_RELOAD.register((server, serverResourceManager, success) -> {
+            VillagerTradesHandler.broadcastErrors(server);
+        });
+
+        NUMISMATIC_GROUP.initialize();
+
+        if (CONFIG.generateCurrencyInChests()) {
+            LootOps.injectItem(NumismaticOverhaulItems.GOLD_COIN, .01f, LootTables.STRONGHOLD_LIBRARY_CHEST, LootTables.BASTION_TREASURE_CHEST, LootTables.STRONGHOLD_CORRIDOR_CHEST,
+                    LootTables.PILLAGER_OUTPOST_CHEST, LootTables.BURIED_TREASURE_CHEST, LootTables.SIMPLE_DUNGEON_CHEST, LootTables.ABANDONED_MINESHAFT_CHEST);
+
+            LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+                if (anyMatch(id, LootTables.DESERT_PYRAMID_CHEST)) {
+                    tableBuilder.pool(LootPool.builder().with(MoneyBagLootEntry.builder(CONFIG.lootOptions.desertMinLoot(), CONFIG.lootOptions.desertMaxLoot()))
+                            .conditionally(RandomChanceLootCondition.builder(0.45f)));
+                } else if (anyMatch(id, LootTables.SIMPLE_DUNGEON_CHEST, LootTables.ABANDONED_MINESHAFT_CHEST)) {
+                    tableBuilder.pool(LootPool.builder().with(MoneyBagLootEntry.builder(CONFIG.lootOptions.dungeonMinLoot(), CONFIG.lootOptions.dungeonMaxLoot()))
+                            .conditionally(RandomChanceLootCondition.builder(0.75f)));
+                } else if (anyMatch(id, LootTables.BASTION_TREASURE_CHEST, LootTables.STRONGHOLD_CORRIDOR_CHEST, LootTables.PILLAGER_OUTPOST_CHEST, LootTables.BURIED_TREASURE_CHEST)) {
+                    tableBuilder.pool(LootPool.builder().with(MoneyBagLootEntry.builder(CONFIG.lootOptions.structureMinLoot(), CONFIG.lootOptions.structureMaxLoot()))
+                            .conditionally(RandomChanceLootCondition.builder(0.75f)));
+                } else if (anyMatch(id, LootTables.STRONGHOLD_LIBRARY_CHEST)) {
+                    tableBuilder.pool(LootPool.builder().with(MoneyBagLootEntry.builder(CONFIG.lootOptions.strongholdLibraryMinLoot(), CONFIG.lootOptions.strongholdLibraryMaxLoot()))
+                            .conditionally(RandomChanceLootCondition.builder(0.85f)));
+                }
+            });
+        }
+    }*/
+}
